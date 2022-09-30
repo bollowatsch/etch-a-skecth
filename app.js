21a6e1ea23e00c1368325359654492804ed7ec81
bollowatsch <stephan.bollauf@gmail.com>
2022-09-30 17:34:46 +0200

    add colorChange at mouseover event

--- app.js
+++ app.js
@@ -3,5 +3,10 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
   const square = document.createElement("div");
   square.classList.toggle("square");
+  square.addEventListener("mouseover", changeColor)
   container.appendChild(square);
+}
+
+function changeColor(e) {
+  e.target.style.backgroundColor = "red";
 }
