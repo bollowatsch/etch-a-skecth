16e25426356d39a918e851268107bc13ca1c538e
bollowatsch <stephan.bollauf@gmail.com>
2022-10-02 13:32:40 +0200

    change button functionality top form input

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const container = document.querySelector(".container");
-gridsizeBtn = document.querySelector("#gridsize");
-gridsizeBtn.addEventListener("click", resetGrid);
+gridSizeForm = document.querySelector("#gridSizeForm");
+gridSizeForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (Number.isNaN(parseInt(gridSize.value))) return;
+  resetGrid();
+});
+gridSize = document.querySelector("#gridSize");
 
 //initial setup (10 by 10)
 setGrid();
 
-function resetGrid() {
-  let size = prompt("Enter a valid grid size (not greater than 100)");
-  if (!(size > 0 && size <= 100)) alert("invalid grid size, please try again");
+function resetGrid(size = 20) {
+  size = parseInt(gridSize.value);
   container.innerHTML = "";
   setGrid(size);
 }
@@ -16,7 +20,7 @@ function changeColor(e) {
   e.target.style.backgroundColor = "#A5C9CA";
 }
 
-function setGrid(size = 30) {
+function setGrid(size = 20) {
   for (let i = 0; i < size * size; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
